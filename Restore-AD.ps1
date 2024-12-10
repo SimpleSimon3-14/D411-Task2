56c19795ab9dba98afc3aab16ae4fc1a05d4e1b6
SimpleSimon3-14 <73441706+SimpleSimon3-14@users.noreply.github.com>
2024-12-10 15:30:24 -0600

    Ready For test

--- Restore-AD.ps1
+++ Restore-AD.ps1
@@ -2,17 +2,31 @@
 #New OU path to add to AD
 $newFinanceOU = “ou=Finance,dc=consultingfirm,dc=com”
 
-#Check AD for OU named Finance
-if ([adsi]::Exists("LDAP://$newFinanceOU)) {
+#Check AD for OU named Finance & output if OU exists, delete OU and Output message that OU was deleted
+if ([adsi]::Exists("LDAP://$newFinanceOU")) {
   Write-Host -Foreground-Color Red "Finance OU Currently Exists"
   Remove-ADOrganizationalUnit -Identity $newFinanceOU -Recursive -confirm:$false
   Write-Host -Foreground-Color Yellow "Finance OU Deleted"
 }
 
-#output if OU exists
-#If OU Finance Exists Delete OU and Output message that OU was deleted
 #Create OU named Finance; output message that OU was created
+New-ADOrganizationalUnit -Name Finance -ProtectedFromAccidentalDletion $false
+Write-Host -ForegroundColor Green "Finance OU ($newFinanceOU) created"
 #Import CSV financePersonnel.csv into AD and directly to Finance OU
-  #Required Properties: First Name, Last Name, Display Name (First Name [space] Last Name), postal code, Office Phone, Mobile Phone
+$FinanceADUSers = Import-CSV $PSScriptRoot\financePersonnel.csv
+ #Required Properties: First Name, Last Name, Display Name (First Name [space] Last Name), postal code, Office Phone, Mobile Phone
+foreach ($FinanceUser in $FinanceADUSers)
+{
+  $FName = $FinanceUser.First_Name
+  $LName = $FinanceUser.Last_Name
+  $DName = $FName + " " + $LName
+  $SamName = $FinanceUser.samAccount
+  $PostalCode = $FinanceUser.PostalCode
+  $OfficeNum = $FinanceUser.OfficePhone
+  $MobileNum = $FinanceUser.MobilePhone
+
+New-AdUser -GivenName $FName -Surname $LName -SamAccountName $SamName -DisplayName $DName -PostalCode $PostalCode -OfficePhone $OfficeNum  -MobilePhone $MobileNum -Path $newFinanceOU
+}
 
 #Generate output file: Get-ADUser -Filter * -SearchBase “ou=Finance,dc=consultingfirm,dc=com” -Properties DisplayName,PostalCode,OfficePhone,MobilePhone > .\AdResults.txt
+Get-ADUser -Filter * -SearchBase “ou=Finance,dc=consultingfirm,dc=com” -Properties DisplayName,PostalCode,OfficePhone,MobilePhone > .\AdResults.txt
