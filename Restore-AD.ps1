0835550a1bcb1fa3db1190b585d8226156d3dc58
SimpleSimon3-14 <73441706+SimpleSimon3-14@users.noreply.github.com>
2024-12-10 14:18:33 -0600

    Update Restore-AD.ps1

--- Restore-AD.ps1
+++ Restore-AD.ps1
@@ -1,1 +1,12 @@
 
+#New OU path to add to AD
+$newFinanceOU = “ou=Finance,dc=consultingfirm,dc=com”
+
+#Check AD for OU named Finance
+#output if OU exists
+#If OU Finance Exists Delete OU and Output message that OU was deleted
+#Create OU named Finance; output message that OU was created
+#Import CSV financePersonnel.csv into AD and directly to Finance OU
+  #Required Properties: First Name, Last Name, Display Name (First Name [space] Last Name), postal code, Office Phone, Mobile Phone
+
+#Generate output file: Get-ADUser -Filter * -SearchBase “ou=Finance,dc=consultingfirm,dc=com” -Properties DisplayName,PostalCode,OfficePhone,MobilePhone > .\AdResults.txt
