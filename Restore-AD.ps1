8552c68f40291b3aa1efe02c42073fc4388bba88
SimpleSimon3-14 <73441706+SimpleSimon3-14@users.noreply.github.com>
2024-12-10 15:47:40 -0600

    Attempt to eliminate variables

--- Restore-AD.ps1
+++ Restore-AD.ps1
@@ -13,19 +13,22 @@ if ([adsi]::Exists("LDAP://$newFinanceOU")) {
 New-ADOrganizationalUnit -Name Finance -ProtectedFromAccidentalDletion $false
 Write-Host -ForegroundColor Green "Finance OU ($newFinanceOU) created"
 #Import CSV financePersonnel.csv into AD and directly to Finance OU
-$FinanceADUSers = Import-CSV $PSScriptRoot\financePersonnel.csv
+$FinanceADImport = Import-CSV $PSScriptRoot\financePersonnel.csv
  #Required Properties: First Name, Last Name, Display Name (First Name [space] Last Name), postal code, Office Phone, Mobile Phone
-foreach ($FinanceUser in $FinanceADUSers)
+foreach ($FinanceUser in $FinanceADImport)
 {
-  $FName = $FinanceUser.First_Name
-  $LName = $FinanceUser.Last_Name
-  $DName = $FName + " " + $LName
-  $SamName = $FinanceUser.samAccount
-  $PostalCode = $FinanceUser.PostalCode
-  $OfficeNum = $FinanceUser.OfficePhone
-  $MobileNum = $FinanceUser.MobilePhone
+  #$FName = $FinanceUser.First_Name
+  #$LName = $FinanceUser.Last_Name
+  #$DName = $FName + " " + $LName
+  #$SamName = $FinanceUser.samAccount
+  #$PostalCode = $FinanceUser.PostalCode
+  #$OfficeNum = $FinanceUser.OfficePhone
+  #$MobileNum = $FinanceUser.MobilePhone
+  
+  $Name = $FinanceUser.First_Name + " " + $FinanceUser.Last_Name
 
-New-AdUser -GivenName $FName -Surname $LName -Name $DName -SamAccountName $SamName -DisplayName $DName -PostalCode $PostalCode -OfficePhone $OfficeNum  -MobilePhone $MobileNum -Path $newFinanceOU
+#New-AdUser -GivenName $FName -Surname $LName -Name $DName -SamAccountName $SamName -DisplayName $DName -PostalCode $PostalCode -OfficePhone $OfficeNum  -MobilePhone $MobileNum -Path $newFinanceOU
+New-AdUser -GivenName $FinanceUser.First_Name -Surname $FinanceUser.Last_Name -Name $Name -SamAccountName $FinanceUser.samAccount -DisplayName $Name -PostalCode $FinanceUser.PostalCode -OfficePhone $FinanceUser.OfficePhone  -MobilePhone $FinanceUser.MobilePhone -Path $newFinanceOU
 }
 
 #Generate output file: Get-ADUser -Filter * -SearchBase “ou=Finance,dc=consultingfirm,dc=com” -Properties DisplayName,PostalCode,OfficePhone,MobilePhone > .\AdResults.txt
