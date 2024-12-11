641a284b4c7284018ff5b66b52b752e171a389e7
SimpleSimon3-14 <73441706+SimpleSimon3-14@users.noreply.github.com>
2024-12-10 19:19:46 -0600

    sql script

--- Restore-SQL.ps1
+++ Restore-SQL.ps1
@@ -1,1 +1,9 @@
+# creating and importing records for Database Restore
+$SQLServerName = ".\SQLEXPRESS"
+$DBName = "ClientDB"
+$tableName = 'dbo.client_a_contacts'
+Invoke-Sqlcmd -ServerInstance $SQLServerName -InputFile $PSScriptRoot\dbcreate.sql -Verbose
+Invoke-Sqlcmd -ServerInstance $SQLServerName -Database $DBName -InputFile $PSScriptRoot\client_a_contacts.sql -Verbose
+Invoke-Sqlcmd -ServerInstance $SQLServerName -Database $DBName -InputFile $PSScriptRoot\import.sql -Variable importPath="'$PSScriptRoot\NewClientData.csv'" -Verbose
 
+Invoke-Sqlcmd -Database $DBName -ServerInstance $SQLServerName -Query 'SELECT * FROM dbo.Client_A_Contacts' > $PSScriptRoot\SqlResults.txt
